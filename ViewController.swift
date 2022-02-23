1acfe7f1c1dbe1ea8c38b4bc83a2930c3ff6a9bc
YSLee-Dev <ysleedev01@gmail.com>
2022-02-23 18:35:11 +0900

    프로젝트 생성

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  10_Covid19_chart
+//
+//  Created by 이윤수 on 2022/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
